f415c3156ef248d4b44edccdd5fb0c3d9a44847a
Guilherme Vieira <guivpw68@gmail.com>
2021-07-26 20:36:40 -0300

    feat: add fastify app bootstrap

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { NestConfig } from 'main/config'
+import { AppModule } from './app.module'
+
+const bootstrap = async () => {
+	const app = await NestFactory.create(AppModule)
+	const configService = app.get(ConfigService)
+	const nestConfig = configService.get<NestConfig>('nest')
+
+	const port = nestConfig?.port || 5002
+
+	await app.listen(port, () => {
+		Logger.log(`App runing at port: ${port}`)
+	})
+}
+
+bootstrap()
