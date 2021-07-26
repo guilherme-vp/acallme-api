f74cd7c9fb4c2212644b81b7fee4ba6f687712a3
Guilherme Vieira <guivpw68@gmail.com>
2021-07-26 19:40:40 -0300

    chore: add jest configuration file

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest/utils'
+import { compilerOptions } from './tsconfig.json'
+
+const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
+	prefix: '<rootDir>/src'
+})
+
+export default {
+	roots: ['<rootDir>/src'],
+	preset: 'ts-jest',
+	transform: {
+		'.+\\.ts$': 'ts-jest'
+	},
+	testEnvironment: 'node',
+	collectCoverage: true,
+	coverageDirectory: './coverage',
+	clearMocks: true,
+	cache: true,
+	moduleNameMapper
+} as Config.InitialOptions
