9c02bc9e32f37711969dc0b5243b765c375826fd
Guilherme Vieira <guivpw68@gmail.com>
2021-07-26 20:41:45 -0300

    refactor: change prisma service to database service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { NODE_ENV } from 'constants/configuration'
 import { config } from 'main/config'
+import { DatabaseModule } from 'services'
 
 const IS_DEV = NODE_ENV === 'development'
 
 @Module({
 	imports: [
+		DatabaseModule,
 		GraphQLModule.forRoot({
 			autoSchemaFile: true,
 			cors: '*',
