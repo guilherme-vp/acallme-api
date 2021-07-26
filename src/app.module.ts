87b0f8ba28044dcd0cb131efca77f85ddae05c86
Guilherme Vieira <guivpw68@gmail.com>
2021-07-26 20:36:27 -0300

    feat: add graphql module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { NODE_ENV } from 'constants/configuration'
 import { config } from 'main/config'
+
+const IS_DEV = NODE_ENV === 'development'
 
 @Module({
 	imports: [
+		GraphQLModule.forRoot({
+			autoSchemaFile: true,
+			cors: '*',
+			debug: IS_DEV
+		}),
 		ConfigModule.forRoot({ isGlobal: true, load: [config] })
 	]
 })
