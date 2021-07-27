744a769843d852ecda1082e855ae6db0cc01a108
Guilherme Vieira <guivpw68@gmail.com>
2021-07-26 23:55:53 -0300

    style: lint configuration file

--- src/constants/configuration.ts
+++ src/constants/configuration.ts
@@ -1,7 +1,7 @@
 type Envs = 'development' | 'production'
 
-const AUTH_HEADER: string = 'authorization'
+const AUTH_HEADER = 'authorization'
 const SECRET: string = process.env.SECRET || 'secret123'
-const NODE_ENV: Envs = process.env.NODE_ENV as Envs || 'development'
+const NODE_ENV: Envs = (process.env.NODE_ENV as Envs) || 'development'
 
 export { SECRET, NODE_ENV, AUTH_HEADER }
