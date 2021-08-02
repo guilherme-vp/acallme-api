dc418100564dd1eeff1814e93184bfbc1227879d
Guilherme Vieira <guivpw68@gmail.com>
2021-08-02 04:39:37 -0300

    chore: remove user module

--- src/modules/user/repositories/repository.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { UserRepository } from './user'
-
-@Module({
-	providers: [UserRepository]
-})
-export class RepositoryModule {}
--- src/modules/user/user.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { RepositoryModule } from './repositories'
-
-@Module({
-	providers: [RepositoryModule]
-})
-export class UserModule {}
