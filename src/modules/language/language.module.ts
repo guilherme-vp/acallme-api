231729e93e44ac800b747e1e83640fd2db517277
Guilherme Vieira <guivpw68@gmail.com>
2021-08-02 01:11:20 -0300

    feat: add language headers resolvers

--- src/modules/language/language.module.ts
+++ src/modules/language/language.module.ts
@@ -1,6 +1,11 @@
 import path from 'path'
 import { Module, Global } from '@nestjs/common'
-import { I18nModule, I18nJsonParser as LocaleParser } from 'nestjs-i18n'
+import {
+	I18nModule,
+	I18nJsonParser as LocaleParser,
+	HeaderResolver,
+	AcceptLanguageResolver
+} from 'nestjs-i18n'
 
 @Global()
 @Module({
@@ -11,7 +16,8 @@ import { I18nModule, I18nJsonParser as LocaleParser } from 'nestjs-i18n'
 			parserOptions: {
 				path: path.join(__dirname, '/locales/'),
 				watch: true
-			}
+			},
+			resolvers: [new HeaderResolver(['x-custom-lang']), AcceptLanguageResolver]
 		})
 	]
 })
