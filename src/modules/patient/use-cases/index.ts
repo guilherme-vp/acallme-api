c6d2a9982838bb438a7e065d5bb938de31821590
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-30 15:24:35 -0300

    feat: add use cases export

--- src/modules/patient/use-cases/index.ts
+++ src/modules/patient/use-cases/index.ts
@@ -1,1 +1,7 @@
+import { Provider } from '@nestjs/common'
+
 export * from './use-cases.service'
+
+export const UseCases = Object.keys(exports).filter(
+	value => typeof value === 'function' && /UseCase/i.test(value)
+) as unknown as Provider[]
