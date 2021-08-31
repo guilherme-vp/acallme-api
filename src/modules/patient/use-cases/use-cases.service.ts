97cd48ff142e51dadf5e81f7b522e31669d76f7d
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 12:45:54 -0300

    refactor: change services use

--- src/modules/patient/use-cases/use-cases.service.ts
+++ src/modules/patient/use-cases/use-cases.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@nestjs/common'
-import { I18nService } from 'nestjs-i18n'
 
 import { SignUpDto } from '../dtos'
-import { PatientRepository } from '../repositories'
 import { SignUpUseCase } from './sign-up'
 
 @Injectable()
 export class UseCasesService {
-	constructor(
-		private readonly patientRepository: PatientRepository,
-		private readonly languageService: I18nService
-	) {}
+	constructor(private readonly signUpUseCase: SignUpUseCase) {}
 
 	async signUp(input: SignUpDto) {
-		const handler = new SignUpUseCase(this.patientRepository, this.languageService)
-
-		return handler.execute(input)
+		return this.signUpUseCase.execute(input)
 	}
 }
