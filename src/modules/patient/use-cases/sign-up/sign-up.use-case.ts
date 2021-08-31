dc908f08caa4cca3815566bae6d529e5100565e2
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 12:18:38 -0300

    feat: create user

--- src/modules/patient/use-cases/sign-up/sign-up.use-case.ts
+++ src/modules/patient/use-cases/sign-up/sign-up.use-case.ts
@@ -2,24 +2,30 @@ import { Injectable } from '@nestjs/common'
 import { I18nService } from 'nestjs-i18n'
 
 import { BaseUseCase } from '~domain/base'
+import { PatientModel } from '~domain/models'
 import { SignUpDto } from '~modules/patient/dtos'
-import { PatientModel } from '~modules/patient/graphql/models'
 import { PatientRepository } from '~modules/patient/repositories'
+import { CryptService } from '~services/crypt'
 
 @Injectable()
 export class SignUpUseCase implements BaseUseCase<PatientModel> {
 	constructor(
 		private readonly patientRepository: PatientRepository,
-		private readonly languageService: I18nService
+		private readonly languageService: I18nService,
+		private readonly cryptService: CryptService
 	) {}
 
 	async execute(input: SignUpDto) {
-		const { email, cpf } = input
+		const { email, cpf, password: userPassword, ...fields } = input
 
 		const patientExists = await this.patientRepository.existsEmailCpf({ email, cpf })
 
 		if (patientExists) {
 			throw new Error(await this.languageService.translate('auth.user-already-exists'))
 		}
+
+		const password = await this.cryptService.encrypt(userPassword)
+
+		return this.patientRepository.create({ ...fields, email, cpf, password })
 	}
 }
