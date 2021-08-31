682889a623d2b36506afc992c71a4f02ca326018
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 11:48:32 -0300

    refactor: remove service modules and graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { GraphQLModule } from '@nestjs/graphql'
 
-import { NODE_ENV } from '~constants/configuration'
 import { PatientModule } from '~core/modules'
-import { DatabaseModule, LanguageModule } from '~core/services'
+import { ServicesModule } from '~core/services'
 import { config } from '~main/config'
 
-const IS_DEV = NODE_ENV === 'development'
-
 @Module({
-	imports: [
-		DatabaseModule,
-		PatientModule,
-		LanguageModule,
-		GraphQLModule.forRoot({
-			autoSchemaFile: true,
-			cors: '*',
-			debug: IS_DEV,
-			context: ({ req, connection }) => (connection ? { req: connection.context } : { req })
-		}),
-		ConfigModule.forRoot({ isGlobal: true, load: [config] })
-	]
+	imports: [ServicesModule, PatientModule, ConfigModule.forRoot({ isGlobal: true, load: [config] })]
 })
 export class AppModule {}
