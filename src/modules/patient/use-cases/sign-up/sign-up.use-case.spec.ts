d322c7088451ecd85d9961ad71e2631c94df5e73
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 12:19:02 -0300

    test: create user

--- src/modules/patient/use-cases/sign-up/sign-up.use-case.spec.ts
+++ src/modules/patient/use-cases/sign-up/sign-up.use-case.spec.ts
@@ -3,8 +3,9 @@ import faker from 'faker'
 import { I18nService } from 'nestjs-i18n'
 
 import { SignUpUseCase } from './sign-up.use-case'
-import { DatabaseModule, LanguageModule } from '~core/services'
-import { UserGender } from '~modules/common/enums'
+import { CryptService } from '~core/services'
+import { UserGender } from '~domain/enums'
+import { PatientModel } from '~domain/models'
 import { SignUpDto } from '~modules/patient/dtos'
 import { PatientRepository } from '~modules/patient/repositories'
 
@@ -18,18 +19,52 @@ const input: SignUpDto = {
 	phone: faker.phone.phoneNumber()
 }
 
+const hashedPassword = faker.datatype.string()
+
+const patient: PatientModel = {
+	...input,
+	id: faker.datatype.uuid(),
+	isEmailVerified: false,
+	isTourCompleted: false,
+	password: hashedPassword,
+	createdAt: faker.date.past(),
+	updatedAt: faker.date.past()
+}
+
 describe('SignUpUseCase', () => {
 	let patientRepository: PatientRepository
 	let signUpUseCase: SignUpUseCase
 	let languageService: I18nService
+	let cryptService: CryptService
 
 	beforeAll(async () => {
 		const module = await Test.createTestingModule({
-			imports: [LanguageModule, DatabaseModule],
-			providers: [SignUpUseCase, PatientRepository]
+			providers: [
+				SignUpUseCase,
+				{
+					provide: I18nService,
+					useValue: {
+						translate: jest.fn()
+					}
+				},
+				{
+					provide: CryptService,
+					useValue: {
+						encrypt: jest.fn()
+					}
+				},
+				{
+					provide: PatientRepository,
+					useValue: {
+						existsEmailCpf: jest.fn(),
+						create: jest.fn()
+					}
+				}
+			]
 		}).compile()
 
 		languageService = await module.get(I18nService)
+		cryptService = await module.get(CryptService)
 		patientRepository = await module.get(PatientRepository)
 		signUpUseCase = await module.get(SignUpUseCase)
 	})
@@ -41,6 +76,7 @@ describe('SignUpUseCase', () => {
 	it('should be defined', () => {
 		expect(patientRepository).toBeDefined()
 		expect(languageService).toBeDefined()
+		expect(cryptService).toBeDefined()
 		expect(signUpUseCase).toBeDefined()
 	})
 
@@ -54,6 +90,28 @@ describe('SignUpUseCase', () => {
 			expect(promise).rejects.toThrowError(await languageService.translate('auth.user-already-exists'))
 			expect(patientRepository.existsEmailCpf).toHaveBeenCalled()
 			expect(patientRepository.existsEmailCpf).toHaveBeenCalledWith({ email: input.email, cpf: input.cpf })
+			expect(languageService.translate).toHaveBeenCalled()
+			expect(languageService.translate).toHaveBeenCalledWith('auth.user-already-exists')
+			expect(cryptService.encrypt).not.toHaveBeenCalled()
+			expect(patientRepository.create).not.toHaveBeenCalled()
+		})
+
+		it('should successfully create a patient and return it', async () => {
+			jest.spyOn(patientRepository, 'existsEmailCpf').mockResolvedValueOnce(0)
+			jest.spyOn(cryptService, 'encrypt').mockResolvedValueOnce(hashedPassword)
+			jest.spyOn(patientRepository, 'create').mockResolvedValueOnce(patient)
+
+			const response = await signUpUseCase.execute(input)
+
+			expect(response).toBeDefined()
+			expect(response).toMatchObject(patient)
+			expect(response.password).toEqual(hashedPassword)
+			expect(patientRepository.existsEmailCpf).toHaveBeenCalled()
+			expect(patientRepository.existsEmailCpf).toHaveBeenCalledWith({ email: input.email, cpf: input.cpf })
+			expect(languageService.translate).not.toHaveBeenCalled()
+			expect(cryptService.encrypt).toHaveBeenCalled()
+			expect(cryptService.encrypt).toHaveBeenCalledWith(input.password)
+			expect(patientRepository.create).toHaveBeenCalledWith({ ...input, password: hashedPassword })
 		})
 	})
 })
