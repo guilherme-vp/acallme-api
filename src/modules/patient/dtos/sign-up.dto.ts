7448da14b2cfee7fcc1e8b6043dbc1e086109ea7
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 12:01:47 -0300

    refactor: remove graphql fields

--- src/modules/patient/dtos/sign-up.dto.ts
+++ src/modules/patient/dtos/sign-up.dto.ts
@@ -1,4 +1,3 @@
-import { Field, InputType } from '@nestjs/graphql'
 import {
 	IsDate,
 	IsEmail,
@@ -10,38 +9,29 @@ import {
 	Matches
 } from 'class-validator'
 
-import { UserGender } from '~modules/common/enums'
+import { UserGender } from '~domain/enums'
 
-@InputType()
 export class SignUpDto {
 	@IsEmail()
-	@Field()
 	email!: string
 
 	@IsString()
 	@Length(2, 50)
-	@Field()
 	name!: string
 
 	@Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/)
-	@Field()
 	password!: string
 
 	@Matches(/?[0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2}{11}/)
-	@Field(() => String)
 	cpf!: string
 
 	@IsEnum(UserGender)
-	@Field(() => UserGender)
 	gender!: UserGender
 
 	@IsDate()
-	@IsOptional()
-	@Field(() => Date, { nullable: true })
-	birth?: Date
+	birth!: Date
 
 	@IsPhoneNumber('BR')
 	@IsOptional()
-	@Field({ nullable: true })
 	phone?: string
 }
