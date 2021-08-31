08500002298ea3a490d25e1ac2d4881157c3cbed
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 11:47:31 -0300

    test: add crypt service

---
+++ src/services/crypt/tests/crypt.service.spec.ts
@@ -0,0 +1,62 @@
+import { Test } from '@nestjs/testing'
+import faker from 'faker'
+
+import { CryptService } from '../crypt.service'
+
+describe('CryptService', () => {
+	let cryptService: CryptService
+
+	beforeEach(async () => {
+		const module = await Test.createTestingModule({
+			providers: [CryptService]
+		}).compile()
+
+		cryptService = await module.get(CryptService)
+	})
+
+	afterEach(() => {
+		jest.resetAllMocks()
+	})
+
+	it('should be defined', () => {
+		expect(cryptService).toBeDefined()
+	})
+
+	describe('execute', () => {
+		it('should encrypt string', async () => {
+			const str = faker.datatype.string()
+			const hashedStr = faker.datatype.string()
+
+			// jest.spyOn(cryptService, 'encrypt').mockResolvedValueOnce(hashedStr)
+
+			const promise = cryptService.encrypt(str)
+
+			expect(promise).resolves.toBeDefined()
+			expect(promise).resolves.toEqual(hashedStr)
+		})
+
+		it('should return false if compare is falsy', async () => {
+			const str = faker.datatype.string()
+			const hashedStr = faker.datatype.string()
+
+			jest.spyOn(cryptService, 'compare').mockResolvedValueOnce(false)
+
+			const promise = cryptService.compare(str, hashedStr)
+
+			expect(promise).resolves.toBeDefined()
+			expect(promise).resolves.toBeFalsy()
+		})
+
+		it('should return true if compare is truthy', async () => {
+			const str = faker.datatype.string()
+			const hashedStr = faker.datatype.string()
+
+			jest.spyOn(cryptService, 'compare').mockResolvedValueOnce(true)
+
+			const promise = cryptService.compare(str, hashedStr)
+
+			expect(promise).resolves.toBeDefined()
+			expect(promise).resolves.toBeTruthy()
+		})
+	})
+})
