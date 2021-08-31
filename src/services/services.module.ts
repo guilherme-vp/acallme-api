bab073c7fcdf44726b6286f982380805d1e7d2eb
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-08-31 11:48:13 -0300

    feat: add services module

---
+++ src/services/services.module.ts
@@ -0,0 +1,29 @@
+import { Global, Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+
+import { CryptService } from './crypt'
+import { DatabaseModule } from './database'
+import { LanguageModule } from './language'
+import { SecurityConfig } from '~main/config'
+
+const jwtModule = JwtModule.registerAsync({
+	useFactory: async (configService: ConfigService) => {
+		const securityConfig = configService.get<SecurityConfig>('security')
+		return {
+			secret: securityConfig?.secret,
+			signOptions: {
+				expiresIn: securityConfig?.expiresIn
+			}
+		}
+	},
+	inject: [ConfigService]
+})
+
+@Global()
+@Module({
+	imports: [jwtModule, LanguageModule, DatabaseModule],
+	providers: [CryptService],
+	exports: [CryptService, jwtModule, LanguageModule, DatabaseModule]
+})
+export class ServicesModule {}
