478d87a1c3303521ecc7197c3c1763d53b184479
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-01 00:45:10 -0300

    refactor: change import order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
+import { config } from '@core/config'
+import { PatientModule } from '@core/modules'
+import { ServicesModule } from '@core/services'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-
-import { PatientModule } from '~core/modules'
-import { ServicesModule } from '~core/services'
-import { config } from '~main/config'
 
 @Module({
 	imports: [ServicesModule, PatientModule, ConfigModule.forRoot({ isGlobal: true, load: [config] })]
