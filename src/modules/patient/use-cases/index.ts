86fc6200a28f7bf297e789c16c84a71c87ee2b7c
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-04 23:57:07 -0300

    refactor: remove patient module

--- src/modules/patient/use-cases/index.ts
+++
@@ -1,7 +0,0 @@
-import { Provider } from '@nestjs/common'
-
-export * from './use-cases.service'
-
-export const UseCases = Object.keys(exports).filter(
-	value => typeof value === 'function' && /UseCase/i.test(value)
-) as unknown as Provider[]
