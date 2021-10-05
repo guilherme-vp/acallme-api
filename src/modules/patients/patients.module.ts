c417e957cfda9ec26a37da1842f5ce425d4e3faf
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-05 00:56:54 -0300

    refactor: remove use cases service

--- src/modules/patients/patients.module.ts
+++ src/modules/patients/patients.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common'
 import { ServicesModule } from '@services/services.module'
 
 import { PatientsController } from './patients.controller'
+import { PatientService } from './patients.service'
 import { PatientRepository } from './repositories'
-import { UseCases, UseCasesService } from './use-cases'
+import { UseCases } from './use-cases'
 
 @Module({
 	imports: [ServicesModule],
 	controllers: [PatientsController],
-	providers: [...UseCases, UseCasesService, PatientRepository]
+	providers: [...UseCases, PatientService, PatientRepository]
 })
 export class PatientsModule {}
