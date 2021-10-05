05083516496ce379898c990d4fcc5e402570fb5d
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-05 00:56:36 -0300

    feat: add find by id service method

--- src/modules/patients/patients.service.ts
+++ src/modules/patients/patients.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common'
 
 import { SignUpDto } from './dtos'
-import { SignUpUseCase } from './use-cases'
+import { SignUpUseCase, FindByIdUseCase } from './use-cases'
 
 @Injectable()
 export class PatientService {
-	constructor(private signUpUseCase: SignUpUseCase) {}
+	constructor(private signUpUseCase: SignUpUseCase, private findByIdUseCase: FindByIdUseCase) {}
 
 	async signUp(input: SignUpDto) {
 		return this.signUpUseCase.execute(input)
+	}
+
+	async findById(id: number) {
+		return this.findByIdUseCase.execute(id)
 	}
 }
