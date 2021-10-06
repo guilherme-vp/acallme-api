3af2aca11fd04aff64d23614588b59bb481dab07
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-06 01:16:36 -0300

    feat: improve user auth guard and store data in request

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,23 +1,58 @@
 import { UserToken } from '@domain/base'
+// eslint-disable-next-line import/extensions
+import { PatientService } from '@modules/patients/patients.service'
 import {
 	Injectable,
 	CanActivate,
 	ExecutionContext,
-	UnauthorizedException
+	HttpException,
+	HttpStatus
 } from '@nestjs/common'
-import { decode } from 'jsonwebtoken'
-import { Observable } from 'rxjs'
+import { JwtService } from '@nestjs/jwt'
+import { I18nService } from 'nestjs-i18n'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+	constructor(
+		private readonly jwtService: JwtService,
+		private readonly languageService: I18nService,
+		private readonly patientService: PatientService
+	) {}
+
+	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const request = context.switchToHttp().getRequest()
 
-		const token = request.headers['authorization'] as string
+		const authHeader = request.headers['authorization']
 
-		const user = decode(token?.split(' ')[1]) as unknown as UserToken
+		const splitted = (authHeader as string).split(' ')
 
-		if (!user) throw new UnauthorizedException()
+		if (!authHeader || !splitted[1]) {
+			throw new HttpException(
+				await this.languageService.translate('auth.not-authorized'),
+				HttpStatus.UNAUTHORIZED
+			)
+		}
+
+		const token = splitted[1]
+		const decoded: UserToken = await this.jwtService.verifyAsync(token)
+
+		if (!decoded) {
+			throw new HttpException(
+				await this.languageService.translate('auth.not-authorized'),
+				HttpStatus.UNAUTHORIZED
+			)
+		}
+
+		const user = await this.patientService.findById(decoded.id)
+
+		if (!user) {
+			throw new HttpException(
+				await this.languageService.translate('auth.user-does-not-exists'),
+				HttpStatus.UNAUTHORIZED
+			)
+		}
+
+		request.user = user.patient
 
 		return true
 	}
