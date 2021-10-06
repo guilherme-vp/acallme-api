d8c40eed55acdc06ff40dc58160d131c50e96208
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-06 00:34:43 -0300

    feat: add login use case to patient service

--- src/modules/patients/patients.service.ts
+++ src/modules/patients/patients.service.ts
@@ -1,20 +1,25 @@
 import { RequireAtLeastOne } from '@core/types'
 import { Injectable } from '@nestjs/common'
 
-import { SignUpDto } from './dtos'
+import { SignUpDto, LoginDto } from './dtos'
 import { PatientModel } from './entities'
-import { SignUpUseCase, FindByIdUseCase, FindOneUseCase } from './use-cases'
+import { SignUpUseCase, LoginUseCase, FindByIdUseCase, FindOneUseCase } from './use-cases'
 
 @Injectable()
 export class PatientService {
 	constructor(
 		private signUpUseCase: SignUpUseCase,
+		private loginUseCase: LoginUseCase,
 		private findByIdUseCase: FindByIdUseCase,
 		private findOneUseCase: FindOneUseCase
 	) {}
 
 	async signUp(input: SignUpDto) {
 		return this.signUpUseCase.execute(input)
+	}
+
+	async login(input: LoginDto) {
+		return this.loginUseCase.execute(input)
 	}
 
 	async findById(id: number) {
