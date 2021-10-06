759ea496f5ac6a80c504307ac1f22b58193a239f
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-06 01:46:39 -0300

    feat: export patient service and use cases

--- src/modules/patients/patients.module.ts
+++ src/modules/patients/patients.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { ServicesModule } from '@services/services.module'
 
 import { PatientsController } from './patients.controller'
@@ -6,9 +6,11 @@ import { PatientService } from './patients.service'
 import { PatientRepository } from './repositories'
 import { UseCases } from './use-cases'
 
+@Global()
 @Module({
 	imports: [ServicesModule],
 	controllers: [PatientsController],
-	providers: [...UseCases, PatientService, PatientRepository]
+	providers: [...UseCases, PatientService, PatientRepository],
+	exports: [...UseCases, PatientService]
 })
 export class PatientsModule {}
