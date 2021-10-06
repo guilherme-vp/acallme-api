45f56ec41c66db548142e1a11de355230936ae3c
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-06 02:26:09 -0300

    refactor: remove classes

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,12 +1,12 @@
 import { UserGender } from '@domain/enums'
 
-export class UserModel {
-	DS_EMAIL!: string
+export interface UserModel {
+	DS_EMAIL: string
 	DS_SENHA?: string
-	DT_NASCIMENTO!: Date
-	DS_GENERO!: UserGender
-	NR_CPF!: number
-	NR_CPF_DIGITO!: number
+	DT_NASCIMENTO: Date
+	DS_GENERO: UserGender
+	NR_CPF: number
+	NR_CPF_DIGITO: number
 	NR_TELEFONE?: number
 	NR_TELEFONE_DDD?: number
 }
--- src/modules/patients/entities/patient.entity.ts
+++ src/modules/patients/entities/patient.entity.ts
@@ -1,7 +1,7 @@
 import { UserModel } from '@domain/entities'
 
-export class PatientModel extends UserModel {
+export interface PatientModel extends UserModel {
 	CD_PACIENTE?: number
 	CD_AGENDA_PACIENTE?: number
-	NM_PACIENTE!: string
+	NM_PACIENTE: string
 }
