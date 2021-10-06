77d26bb27b24c466ec8ee471693bd492c894088b
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-06 00:36:06 -0300

    feat: add login and me controllers and roles and auth guards

--- src/modules/patients/patients.controller.ts
+++ src/modules/patients/patients.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common'
+import { Roles } from '@core/decorators'
+import { AuthGuard } from '@core/guards'
+import { Role } from '@domain/enums'
+import { Controller, Get, Post, Body, Param, Query, UseGuards } from '@nestjs/common'
 
-import { SignUpDto } from './dtos'
+import { Patient } from './decorators'
+import { LoginDto, SignUpDto } from './dtos'
 import { PatientModel } from './entities'
 import { PatientService } from './patients.service'
 
@@ -13,11 +17,26 @@ export class PatientsController {
 		return this.patientService.signUp(input)
 	}
 
+	@Post('login')
+	async login(@Body() input: LoginDto) {
+		return this.patientService.login(input)
+	}
+
+	@UseGuards(AuthGuard)
+	@Get('me')
+	async me(@Patient('id') id: number) {
+		return this.patientService.findById(id)
+	}
+
+	@UseGuards(AuthGuard)
+	@Roles(Role.Patient, Role.Specialist)
 	@Get(':id')
 	async findById(@Param('id') id: string) {
 		return this.patientService.findById(+id)
 	}
 
+	@UseGuards(AuthGuard)
+	@Roles(Role.Patient, Role.Specialist)
 	@Get()
 	async findOne(@Query() query: PatientModel) {
 		return this.patientService.findOne(query)
