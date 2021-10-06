ad16ff3f2653201bcb93eb73457afde2561a1a08
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-06 01:47:00 -0300

    feat: add specialist module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { PatientsModule } from './modules/patients/patients.module'
+import { SpecialistsModule } from './modules/specialists/specialists.module'
 
 @Module({
 	imports: [
 		ServicesModule,
 		PatientsModule,
+		SpecialistsModule,
 		ConfigModule.forRoot({ isGlobal: true, load: [config] })
 	]
 })
