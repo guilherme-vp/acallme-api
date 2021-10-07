1f0cc284ac6976d8e51be93654f9d6a6b73c45fd
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-07 01:57:08 -0300

    feat: add email attachments

--- src/providers/mails.ts
+++ src/providers/mails.ts
@@ -1,9 +1,23 @@
+import path from 'path'
+
 import { IReturnMail } from '@core/types/email-args'
 import { WelcomeMailVars, generateWelcomeMail } from '@core/views/mails'
 
 export const welcomeEmailProps = (args: WelcomeMailVars): IReturnMail => {
 	return {
 		subject: 'Estamos felizes que você está conosco!',
-		html: generateWelcomeMail(args)
+		html: generateWelcomeMail(args),
+		attachments: [
+			{
+				filename: 'clock.png',
+				cid: 'clock',
+				path: path.resolve(__dirname, '..', 'assets', 'images', 'clock.png')
+			},
+			{
+				filename: 'heart.png',
+				cid: 'heart',
+				path: path.resolve(__dirname, '..', 'assets', 'images', 'heart.png')
+			}
+		]
 	}
 }
