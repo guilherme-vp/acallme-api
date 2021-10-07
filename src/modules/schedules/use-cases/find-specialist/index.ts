7291bd89a9f089173f1c260340abea33da15be44
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-07 17:42:15 -0300

    feat: add get schedule by specialist id

---
+++ src/modules/schedules/use-cases/find-specialist/index.ts
@@ -0,0 +1,1 @@
+export * from './find-specialist.use-case'
--- src/modules/schedules/use-cases/index.ts
+++ src/modules/schedules/use-cases/index.ts
@@ -1,6 +1,8 @@
 import { Provider } from '@nestjs/common'
 
 export * from './create'
+export * from './find-patient'
+export * from './find-specialist'
 
 export const UseCases = Object.values(exports).filter(
 	value => typeof value === 'function' && /UseCase/i.test(value.name)
