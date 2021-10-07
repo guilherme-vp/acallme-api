83647cbc643b3d0eecd09a28c057989a83611ec7
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-07 01:00:58 -0300

    feat: add mail module

--- src/services/mail/mail.module.ts
+++ src/services/mail/mail.module.ts
@@ -1,20 +1,27 @@
-import { MailerConfig } from '@core/config'
+import { MailerConfig } from '@common/config'
 import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer'
-import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { Global, Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
+import { MailerService } from './mail.service'
+
+const mailerModule = NestMailerModule.forRootAsync({
+	imports: [ConfigModule],
+	inject: [ConfigService],
+	useFactory: async (configService: ConfigService) => {
+		const mailerConfig = configService.get<MailerConfig>('mailer')
+		return {
+			transport: mailerConfig?.transport,
+			defaults: mailerConfig?.defaults,
+			preview: mailerConfig?.preview ?? false
+		}
+	}
+})
+
+@Global()
 @Module({
-	imports: [
-		NestMailerModule.forRootAsync({
-			useFactory: async (configService: ConfigService) => {
-				const mailerConfig = configService.get<MailerConfig>('mailer')
-				return {
-					transport: mailerConfig?.transport,
-					defaults: mailerConfig?.defaults,
-					preview: mailerConfig?.preview ?? false
-				}
-			}
-		})
-	]
+	imports: [mailerModule],
+	providers: [MailerService],
+	exports: [mailerModule, MailerService]
 })
 export class MailerModule {}
