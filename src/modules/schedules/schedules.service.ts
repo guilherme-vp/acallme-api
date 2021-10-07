b5be571723debc8d1237405bf7995b1639e81129
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-10-07 17:42:31 -0300

    feat: add get patient and specialist schedules service methods

--- src/modules/schedules/schedules.service.ts
+++ src/modules/schedules/schedules.service.ts
@@ -1,13 +1,26 @@
 import { Role } from '@common/domain/enums'
 import { Injectable } from '@nestjs/common'
 
+import { FindPatientUseCase, FindSpecialistUseCase } from './use-cases'
 import { CreateUseCase } from './use-cases/create'
 
 @Injectable()
 export class SchedulesService {
-	constructor(private readonly createUseCase: CreateUseCase) {}
+	constructor(
+		private readonly createUseCase: CreateUseCase,
+		private readonly findSpecialistUseCase: FindSpecialistUseCase,
+		private readonly findPatientUseCase: FindPatientUseCase
+	) {}
 
 	async create(userId: number, role: Role) {
 		return this.createUseCase.execute(userId, role)
+	}
+
+	async getPatientSchedule(patientId: number) {
+		return this.findPatientUseCase.execute(patientId)
+	}
+
+	async getSpecialistSchedule(specialistId: number) {
+		return this.findSpecialistUseCase.execute(specialistId)
 	}
 }
