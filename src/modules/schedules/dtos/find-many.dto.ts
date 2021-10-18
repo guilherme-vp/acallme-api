13faea5dab74d03bb67bdb4757351efd5edf5a0b
Guilherme Vieira <guivpw68@gmail.com>
2021-10-18 03:06:51 -0300

    feta: add find many schedules dto

---
+++ src/modules/schedules/dtos/find-many.dto.ts
@@ -0,0 +1,27 @@
+import { IsOptional, IsDateString, IsNumber, IsBoolean } from 'class-validator'
+
+export class FindManyDto {
+	@IsDateString()
+	@IsOptional()
+	rangeStart?: string
+
+	@IsDateString()
+	@IsOptional()
+	rangeEnd?: string
+
+	@IsOptional()
+	@IsNumber()
+	appointmentId?: number
+
+	@IsOptional()
+	@IsNumber()
+	specialistId?: number
+
+	@IsOptional()
+	@IsNumber()
+	patientId?: number
+
+	@IsOptional()
+	@IsBoolean()
+	confirmed?: boolean
+}
