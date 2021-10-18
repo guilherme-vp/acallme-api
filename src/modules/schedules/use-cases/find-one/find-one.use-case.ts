3c39e5ee3365bda4541b125c63b744bcb4bb3d5c
Guilherme Vieira <guivpw68@gmail.com>
2021-10-18 14:38:39 -0300

    feat: add find one schedule use case

---
+++ src/modules/schedules/use-cases/find-one/find-one.use-case.ts
@@ -0,0 +1,51 @@
+import { RequireAtLeastOne } from '@core/types'
+import { FindOneDto } from '@modules/schedules/dtos'
+import { ScheduleFormatted, ScheduleModel } from '@modules/schedules/entities'
+import { ScheduleRepository } from '@modules/schedules/repositories'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { I18nService } from 'nestjs-i18n'
+
+@Injectable()
+export class FindOneUseCase {
+	constructor(
+		private readonly scheduleRepository: ScheduleRepository,
+		private readonly languageService: I18nService
+	) {}
+
+	async execute(where: FindOneDto): Promise<{ schedule: ScheduleFormatted } | null> {
+		const keys: RequireAtLeastOne<ScheduleModel> | undefined = undefined
+
+		if (where.appointmentId) {
+			keys!.CD_CHAMADA = where.appointmentId
+		}
+		if (where.patientId) {
+			keys!.CD_PACIENTE = where.patientId
+		}
+		if (where.specialistId) {
+			keys!.CD_ESPECIALISTA = where.specialistId
+		}
+		if (where.confirmed) {
+			keys!.VL_CONFIRMADO = where.confirmed ? 1 : 0
+		}
+		if (where.rangeEnd) {
+			keys!.DT_FIM_RANGE = new Date(where.rangeEnd)
+		}
+		if (where.rangeStart) {
+			keys!.DT_INI_RANGE = new Date(where.rangeStart)
+		}
+
+		if (!keys) {
+			throw new BadRequestException(await this.languageService.translate('no-field'))
+		}
+
+		const foundSchedule = await this.scheduleRepository.getOne(keys, 'OR')
+
+		if (!foundSchedule) {
+			return null
+		}
+
+		return {
+			schedule: foundSchedule
+		}
+	}
+}
--- src/modules/schedules/use-cases/index.ts
+++ src/modules/schedules/use-cases/index.ts
@@ -4,6 +4,7 @@ export * from './create'
 export * from './confirm'
 export * from './find-by-id'
 export * from './find-many'
+export * from './find-one'
 
 export const UseCases = Object.values(exports).filter(
 	value => typeof value === 'function' && /UseCase/i.test(value.name)
