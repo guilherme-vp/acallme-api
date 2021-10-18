a08f50acb8ec556b8cefc2412601ce79c139a68e
Guilherme Vieira <guivpw68@gmail.com>
2021-10-18 03:07:37 -0300

    feat: add find many schedules use case

---
+++ src/modules/schedules/use-cases/find-many/find-many.use-case.ts
@@ -0,0 +1,37 @@
+import { RequireAtLeastOne } from '@core/types'
+import { FindManyDto } from '@modules/schedules/dtos'
+import { ScheduleFormatted, ScheduleModel } from '@modules/schedules/entities'
+import { ScheduleRepository } from '@modules/schedules/repositories'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class FindManyUseCase {
+	constructor(private readonly scheduleRepository: ScheduleRepository) {}
+
+	async execute(where: FindManyDto): Promise<ScheduleFormatted[]> {
+		let keys: RequireAtLeastOne<ScheduleModel> | undefined
+
+		if (where.appointmentId) {
+			keys!.CD_CHAMADA = where.appointmentId
+		}
+		if (where.patientId) {
+			keys!.CD_PACIENTE = where.patientId
+		}
+		if (where.specialistId) {
+			keys!.CD_ESPECIALISTA = where.specialistId
+		}
+		if (where.confirmed) {
+			keys!.VL_CONFIRMADO = where.confirmed ? 1 : 0
+		}
+		if (where.rangeEnd) {
+			keys!.DT_FIM_RANGE = new Date(where.rangeEnd)
+		}
+		if (where.rangeStart) {
+			keys!.DT_INI_RANGE = new Date(where.rangeStart)
+		}
+
+		const foundSchedules = await this.scheduleRepository.getMany(keys)
+
+		return foundSchedules
+	}
+}
