18f91e4ca5e996f39c018e575e7c7fff86413913
Guilherme Vieira <guivpw68@gmail.com>
2021-10-18 03:07:06 -0300

    refactor: update create schedule logic

--- src/modules/schedules/use-cases/create/create.use-case.ts
+++ src/modules/schedules/use-cases/create/create.use-case.ts
@@ -1,34 +1,60 @@
-import { Role } from '@common/domain/enums'
+import { PatientRepository } from '@modules/patients/repositories'
+import { CreateDto } from '@modules/schedules/dtos'
 import { ScheduleFormatted } from '@modules/schedules/entities'
-import {
-	PatientScheduleRepository,
-	SpecialistScheduleRepository
-} from '@modules/schedules/repositories'
-import { formatSchedule } from '@modules/schedules/utils'
-import { Injectable } from '@nestjs/common'
+import { ScheduleRepository } from '@modules/schedules/repositories'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { differenceInDays } from 'date-fns'
+import { I18nService } from 'nestjs-i18n'
 
 @Injectable()
 export class CreateUseCase {
 	constructor(
-		private readonly patientSchedRepository: PatientScheduleRepository,
-		private readonly specialistSchedRepository: SpecialistScheduleRepository
+		private readonly scheduleRepository: ScheduleRepository,
+		private readonly patientRepository: PatientRepository,
+		private readonly languageService: I18nService
 	) {}
 
-	async execute(userId: number, role: Role): Promise<ScheduleFormatted> {
-		if (role === Role.Patient) {
-			const schedule = await this.patientSchedRepository.create({ CD_PACIENTE: userId })
+	async execute(specialistId: number, data: CreateDto): Promise<ScheduleFormatted> {
+		const { dateEnd, dateStart } = data
+		const rangeStart = new Date(dateStart)
+		const rangeEnd = new Date(dateEnd)
 
-			const finalSchedule = formatSchedule(schedule)
+		if (data.patientId) {
+			const patient = await this.patientRepository.getOneById(data.patientId)
 
-			return finalSchedule
+			if (!patient) {
+				throw new BadRequestException(
+					this.languageService.translate('patient.patient-not-found')
+				)
+			}
 		}
 
-		const schedule = await this.specialistSchedRepository.create({
-			CD_SPECIALIST: userId
+		const now = new Date()
+
+		if (differenceInDays(new Date(dateStart), now) < 1) {
+			throw new BadRequestException(
+				this.languageService.translate('schedule.date-not-valid')
+			)
+		}
+
+		const scheduleExist = await this.scheduleRepository.existsAtTheSameTime(
+			rangeStart,
+			specialistId
+		)
+
+		if (scheduleExist) {
+			throw new BadRequestException(
+				this.languageService.translate('schedule.date-not-valid')
+			)
+		}
+
+		const createdSchedule = await this.scheduleRepository.create({
+			CD_ESPECIALISTA: specialistId,
+			DT_INI_RANGE: rangeStart,
+			DT_FIM_RANGE: rangeEnd,
+			VL_CONFIRMADO: 0
 		})
 
-		const finalSchedule = formatSchedule(schedule)
-
-		return finalSchedule
+		return createdSchedule
 	}
 }
