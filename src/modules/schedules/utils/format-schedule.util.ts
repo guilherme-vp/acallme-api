ecd366fe2a85cfeb4ac49a3b9f910de0a766b61c
Guilherme Vieira <guivpw68@gmail.com>
2021-10-19 01:00:02 -0300

    refactor: change fields to match new db

--- src/modules/schedules/utils/format-schedule.util.ts
+++ src/modules/schedules/utils/format-schedule.util.ts
@@ -2,23 +2,25 @@ import { ScheduleFormatted, ScheduleModel } from '../entities'
 
 export function formatSchedule(data: ScheduleModel): ScheduleFormatted {
 	const {
-		CD_AGENDA: scheduleId,
+		CD_AGENDA: id,
 		CD_CHAMADA: appointmentId,
 		CD_ESPECIALISTA: specialistId,
 		CD_PACIENTE: patientId,
 		DT_FIM_RANGE: rangeEnd,
 		DT_INI_RANGE: rangeStart,
-		VL_CONFIRMADO: confirmed
+		VL_CONFIRMADO: confirmed,
+		NR_DESABILITADO: disabled
 	} = data
 
 	const formatted: ScheduleFormatted = {
+		id,
 		appointmentId,
-		scheduleId,
 		patientId,
 		specialistId,
 		rangeStart,
 		rangeEnd,
-		confirmed: Boolean(confirmed)
+		confirmed: Boolean(confirmed),
+		disabled: Boolean(disabled)
 	}
 
 	return formatted
