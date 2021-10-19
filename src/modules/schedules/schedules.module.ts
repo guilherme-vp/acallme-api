d4c63c59f4d033a454d4a747dd97fe0780b432f2
Guilherme Vieira <guivpw68@gmail.com>
2021-10-19 00:59:49 -0300

    feat: add gateway and schedule services and modules

--- src/modules/schedules/schedules.module.ts
+++ src/modules/schedules/schedules.module.ts
@@ -1,14 +1,17 @@
 import { Global, Module } from '@nestjs/common'
+import { ScheduleModule } from '@nestjs/schedule'
 import { ServicesModule } from '@services/services.module'
 
 import { Repositories } from './repositories'
 import { SchedulesService } from './schedules.service'
+import { TaskService } from './tasks'
 import { UseCases } from './use-cases'
+import { Gateways } from './websockets'
 
 @Global()
 @Module({
-	imports: [ServicesModule],
-	providers: [...UseCases, ...Repositories, SchedulesService],
-	exports: [...UseCases, SchedulesService]
+	imports: [ServicesModule, ScheduleModule],
+	providers: [...UseCases, ...Gateways, ...Repositories, TaskService, SchedulesService],
+	exports: [...UseCases, SchedulesService, TaskService]
 })
 export class SchedulesModule {}
