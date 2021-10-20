006e0d38c22947a87b500ae1d329cf12eb2fedb9
Guilherme Vieira <guivpw68@gmail.com>
2021-10-19 21:31:48 -0300

    refactor: remove videocall gateway

--- src/modules/schedules/schedules.module.ts
+++ src/modules/schedules/schedules.module.ts
@@ -1,3 +1,4 @@
+import { CallsModule } from '@modules/calls/calls.module'
 import { PatientsModule } from '@modules/patients/patients.module'
 import { SpecialistsModule } from '@modules/specialists/specialists.module'
 import { forwardRef, Global, Logger, Module } from '@nestjs/common'
@@ -8,24 +9,17 @@ import { Repositories } from './repositories'
 import { SchedulesService } from './schedules.service'
 import { TaskService } from './tasks'
 import { UseCases } from './use-cases'
-import { Gateways } from './websockets'
 
 @Global()
 @Module({
 	imports: [
 		ServicesModule,
 		NestScheduleModule,
+		CallsModule,
 		forwardRef(() => PatientsModule),
 		forwardRef(() => SpecialistsModule)
 	],
-	providers: [
-		...UseCases,
-		...Gateways,
-		...Repositories,
-		TaskService,
-		SchedulesService,
-		Logger
-	],
+	providers: [...UseCases, ...Repositories, TaskService, SchedulesService, Logger],
 	exports: [...UseCases, SchedulesService, TaskService]
 })
 export class SchedulesModule {}
