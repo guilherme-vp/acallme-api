f1f85b2ba81bc79197cd02f246d3a79fbb90ac7f
Guilherme Vieira <guivpw68@gmail.com>
2021-10-19 21:32:22 -0300

    types: add ws schedule and call interfaces

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,8 +1,17 @@
 import { UserFormattedModel } from '@common/domain/entities'
 import { Role } from '@common/domain/enums'
+import { CallFormatted } from '@modules/calls/entities'
+import { ScheduleFormatted } from '@modules/schedules/entities'
 
 declare module 'fastify' {
 	export interface FastifyRequest {
 		user: UserFormattedModel & { role: Role }
+	}
+}
+
+declare module 'socket.io' {
+	export interface Socket {
+		schedule?: ScheduleFormatted
+		call?: CallFormatted
 	}
 }
