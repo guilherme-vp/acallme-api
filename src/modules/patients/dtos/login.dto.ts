9f08af304900c427e0c813e582b3fb8ef52d933f
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 20:50:07 -0300

    feat: verify dto length

--- src/modules/patients/dtos/login.dto.ts
+++ src/modules/patients/dtos/login.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator'
+import { IsString, MinLength } from 'class-validator'
 
 export class LoginDto {
 	@IsString()
+	@MinLength(1)
 	username!: string
 
 	@IsString()
+	@MinLength(1)
 	password!: string
 }
