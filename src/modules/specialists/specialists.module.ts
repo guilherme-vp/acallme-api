f6063916f9a5a60bf3e528a73540f176aa5a5a34
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 22:50:50 -0300

    feat: add specialty repository to modules

--- src/modules/specialists/specialists.module.ts
+++ src/modules/specialists/specialists.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common'
 
-import { SpecialistRepository } from './repositories'
+import { SpecialistRepository, SpecialtyRepository } from './repositories'
 import { SpecialistsController } from './specialists.controller'
 import { SpecialistService } from './specialists.service'
 import { UseCases } from './use-cases'
@@ -8,7 +8,7 @@ import { UseCases } from './use-cases'
 @Global()
 @Module({
 	controllers: [SpecialistsController],
-	providers: [...UseCases, SpecialistService, SpecialistRepository],
+	providers: [...UseCases, SpecialistService, SpecialistRepository, SpecialtyRepository],
 	exports: [SpecialistService]
 })
 export class SpecialistsModule {}
