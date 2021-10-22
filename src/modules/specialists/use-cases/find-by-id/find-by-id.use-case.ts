af9eca22e09042031028a187365bb2189d4521f7
Guilherme Vieira <guivpw68@gmail.com>
2021-10-22 03:30:07 -0300

    feat: get specialist specialties

--- src/modules/specialists/use-cases/find-by-id/find-by-id.use-case.ts
+++ src/modules/specialists/use-cases/find-by-id/find-by-id.use-case.ts
@@ -1,11 +1,17 @@
 import { BaseUseCase } from '@common/domain/base'
 import { SpecialistFormatted, SpecialistModel } from '@modules/specialists/entities'
-import { SpecialistRepository } from '@modules/specialists/repositories'
+import {
+	SpecialistRepository,
+	SpecialtyRepository
+} from '@modules/specialists/repositories'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class FindByIdUseCase implements BaseUseCase<SpecialistModel> {
-	constructor(private readonly specialistRepository: SpecialistRepository) {}
+	constructor(
+		private readonly specialistRepository: SpecialistRepository,
+		private readonly specialtyRepository: SpecialtyRepository
+	) {}
 
 	async execute(id: number): Promise<SpecialistFormatted | null> {
 		const foundSpecialist = await this.specialistRepository.getOneById(id)
@@ -14,8 +20,10 @@ export class FindByIdUseCase implements BaseUseCase<SpecialistModel> {
 			return null
 		}
 
+		const specialties = await this.specialtyRepository.getAllSpecialtiesById(id)
+
 		delete foundSpecialist.password
 
-		return foundSpecialist
+		return { ...foundSpecialist, specialties }
 	}
 }
