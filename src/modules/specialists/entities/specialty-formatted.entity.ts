88d97fd70c285283832ad45d084d0c5d91db43f2
Guilherme Vieira <guivpw68@gmail.com>
2021-10-21 22:49:32 -0300

    feat: add specialistId in models

--- src/modules/specialists/entities/specialty-formatted.entity.ts
+++ src/modules/specialists/entities/specialty-formatted.entity.ts
@@ -3,5 +3,6 @@ import { SpecialistFormatted } from './specialist-formatted.entity'
 export interface SpecialtyFormatted {
 	id: number
 	name: string
+	specialistId?: number
 	specialists?: SpecialistFormatted[]
 }
--- src/modules/specialists/entities/specialty.entity.ts
+++ src/modules/specialists/entities/specialty.entity.ts
@@ -1,4 +1,5 @@
 export interface SpecialtyModel {
 	CD_ESPECIALIDADE: number
 	TP_ESPECIALIDADE: string
+	CD_ESPECIALISTA?: number
 }
