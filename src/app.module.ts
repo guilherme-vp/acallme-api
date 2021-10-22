212dfc8139e1fa37d67813fcec37c2a2b01f0357
Guilherme Vieira <guivpw68@gmail.com>
2021-10-22 01:25:17 -0300

    refactor: remove unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,11 @@ import { PatientsModule } from '@modules/patients/patients.module'
 import { RecordsModule } from '@modules/records/records.module'
 import { SchedulesModule } from '@modules/schedules/schedules.module'
 import { SpecialistsModule } from '@modules/specialists/specialists.module'
-import { Controller, Get, Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ServicesModule } from '@services/services.module'
 
-@Controller()
-class ControllerTop {
-	@Get()
-	top() {
-		return { ok: true }
-	}
-}
-
 @Module({
-	controllers: [ControllerTop],
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true, load: [config] }),
 		ServicesModule,
