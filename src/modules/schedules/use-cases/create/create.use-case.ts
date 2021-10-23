1a77fee9391fca5dac7418032a31087270a10505
Guilherme Vieira <guivpw68@gmail.com>
2021-10-22 22:07:28 -0300

    feat: add logger

--- src/modules/schedules/use-cases/create/create.use-case.ts
+++ src/modules/schedules/use-cases/create/create.use-case.ts
@@ -1,12 +1,14 @@
 import { CreateDto } from '@modules/schedules/dtos'
 import { ScheduleFormatted } from '@modules/schedules/entities'
 import { ScheduleRepository } from '@modules/schedules/repositories'
-import { BadRequestException, Injectable } from '@nestjs/common'
-import { differenceInDays } from 'date-fns'
+import { BadRequestException, Injectable, Logger } from '@nestjs/common'
+import { differenceInHours, startOfHour } from 'date-fns'
 import { I18nService } from 'nestjs-i18n'
 
 @Injectable()
 export class CreateUseCase {
+	private logger: Logger = new Logger()
+
 	constructor(
 		private readonly scheduleRepository: ScheduleRepository,
 		private readonly languageService: I18nService
@@ -14,34 +16,39 @@ export class CreateUseCase {
 
 	async execute(data: CreateDto, patientId: number): Promise<ScheduleFormatted> {
 		const { dateEnd, dateStart, specialistId } = data
-		const rangeStart = new Date(dateStart)
-		const rangeEnd = new Date(dateEnd)
+		const rangeStart = startOfHour(new Date(dateStart))
+		const rangeEnd = startOfHour(new Date(dateEnd))
 
 		const now = new Date()
 
-		if (differenceInDays(new Date(dateStart), now) < 1) {
+		this.logger.log('Getting the difference between hours and date', rangeStart, rangeEnd)
+		if (differenceInHours(new Date(dateStart), now) < 1) {
 			throw new BadRequestException(
 				await this.languageService.translate('schedule.date-not-valid')
 			)
 		}
 
+		this.logger.log('Checking if schedules exists at the same time')
 		const scheduleExist = await this.scheduleRepository.existsAtTheSameTime(
 			rangeStart,
 			specialistId
 		)
 
 		if (scheduleExist) {
+			this.logger.log('Schedule date is not valid')
 			throw new BadRequestException(
 				await this.languageService.translate('schedule.date-not-valid')
 			)
 		}
 
+		this.logger.log('Creating the schedule with confirmed value as false')
 		const createdSchedule = await this.scheduleRepository.create({
 			CD_ESPECIALISTA: specialistId,
 			CD_PACIENTE: patientId,
 			DT_INI_RANGE: rangeStart,
 			DT_FIM_RANGE: rangeEnd,
-			VL_CONFIRMADO: 0
+			VL_CONFIRMADO: 0,
+			NR_DESABILITADO: 1
 		})
 
 		if (!createdSchedule) {
