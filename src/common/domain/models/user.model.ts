212064f2258ee6aba601cfbbed147164046b6488
Guilherme Vieira <guivpw68@gmail.com>
2021-10-26 02:04:44 -0300

    feat: add typeorm columns

---
+++ src/common/domain/models/user.model.ts
@@ -0,0 +1,31 @@
+import { UserGender } from '@common/domain/enums'
+import { Column } from 'typeorm'
+
+export abstract class UserModel {
+	@Column({ name: 'DS_EMAIL', unique: true, type: 'varchar2', length: 50 })
+	email!: string
+
+	@Column({ name: 'DS_SENHA', type: 'varchar2', length: 75 })
+	password?: string
+
+	@Column({ name: 'DT_NASCIMENTO', type: 'date' })
+	birth!: string
+
+	@Column({ name: 'DS_GENERO', type: 'varchar2', length: 2 })
+	gender!: UserGender
+
+	@Column({ name: 'IM_AVATAR_URL', type: 'varchar2', length: 100, nullable: true })
+	avatarUrl?: string
+
+	@Column({ name: 'NR_CPF', type: 'number', precision: 9, nullable: true, unique: true })
+	cpf?: number
+
+	@Column({ name: 'NR_CPF_DIGITO', type: 'number', nullable: true, precision: 2 })
+	cpfDigits?: number
+
+	@Column({ name: 'NR_TELEFONE', type: 'number', precision: 9 })
+	phone!: number
+
+	@Column({ name: 'NR_TELEFONE_DDD', type: 'number', precision: 2 })
+	phoneDigits!: number
+}
