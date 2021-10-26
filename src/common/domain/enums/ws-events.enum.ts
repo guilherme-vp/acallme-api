11f49e45cac964809352989d282c307408778fc8
Guilherme Vieira <guivpw68@gmail.com>
2021-10-26 02:05:19 -0300

    refactor: improve ws event names

--- src/common/domain/enums/ws-events.enum.ts
+++ src/common/domain/enums/ws-events.enum.ts
@@ -1,12 +1,20 @@
 export enum WsEvents {
+	// Profiling
 	ME = 'me',
+
+	// Messages
 	SEND_MESSAGE = 'send-message',
 	RECEIVE_MESSAGE = 'receive-message',
+
+	// Call Update
 	UPDATE_MEDIA = 'update-media',
-	UPDATE_USER_MEDIA = 'update-user-media',
+	RECEIVE_UPDATED_MEDIA = 'receive-update-media',
+
+	// Call
 	ENTER_CALL = 'enter-call',
 	END_CALL = 'end-call',
-	RECEIVE_USER = 'receive-user',
-	SEND_NOTIFICATION = 'send-notification',
-	SEND_CLOSE_NOTIFICATION = 'send-close-notification'
+	SEND_CLOSE_NOTIFICATION = 'send-close-notification',
+
+	// Notifications
+	SEND_NOTIFICATION = 'send-notification'
 }
