a9abd05fd92bf785bcb20823aa51137f70cf6381
Guilherme Vieira <guivpw68@gmail.com>
2021-10-26 02:07:28 -0300

    feat: make schedule a typeorm entity

--- src/modules/schedules/entities/index.ts
+++ src/modules/schedules/entities/index.ts
@@ -1,2 +1,1 @@
 export * from './schedule.entity'
-export * from './schedule-formatted.entity'
--- src/modules/schedules/entities/schedule.entity.ts
+++ src/modules/schedules/entities/schedule.entity.ts
@@ -1,10 +1,71 @@
-export interface ScheduleModel {
-	CD_AGENDA: number
-	CD_CHAMADA?: number
-	CD_ESPECIALISTA: number
-	CD_PACIENTE?: number
-	DT_INI_RANGE: Date
-	DT_FIM_RANGE: Date
-	VL_CONFIRMADO?: number
-	NR_DESABILITADO?: number
+import { Call } from '@modules/calls/entities'
+import { Patient } from '@modules/patients/entities'
+import { Specialist } from '@modules/specialists/entities'
+import {
+	Column,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	OneToOne,
+	PrimaryGeneratedColumn
+} from 'typeorm'
+
+@Entity({
+	name: 'AGENDA',
+	orderBy: {
+		rangeStart: 'ASC'
+	}
+})
+export class Schedule {
+	@PrimaryGeneratedColumn({ name: 'CD_AGENDA', type: 'number' })
+	id!: number
+
+	@Column({ name: 'CD_AGENDA', type: 'number', unique: true, precision: 5, nullable: true })
+	callId?: number
+
+	@Column({ name: 'CD_ESPECIALISTA', type: 'number', precision: 5 })
+	specialistId!: number
+
+	@Column({ name: 'CD_PACIENTE', type: 'number', precision: 5, nullable: true })
+	patientId?: number
+
+	@Column({ name: 'DT_INIT_RANGE', type: 'timestamp' })
+	rangeStart!: Date
+
+	@Column({ name: 'DT_FIM_RANGE', type: 'timestamp' })
+	rangeEnd!: Date
+
+	@Column({
+		name: 'VL_CONFIRMADO',
+		type: 'number',
+		precision: 1,
+		default: 0,
+		nullable: true
+	})
+	confirmed?: number
+
+	@Column({
+		name: 'NR_DESABILITADO',
+		type: 'number',
+		precision: 1,
+		default: 0,
+		nullable: true
+	})
+	disabled?: number
+
+	@OneToOne(() => Call, call => call.scheduleId, { nullable: true, eager: true })
+	@JoinColumn({
+		name: 'agenda_chamada'
+	})
+	call?: Call
+
+	@ManyToOne(() => Specialist, { eager: true })
+	@JoinColumn({
+		name: 'agenda_especialista'
+	})
+	specialist!: Specialist
+
+	@ManyToOne(() => Patient, { nullable: true, eager: true })
+	@JoinColumn({ name: 'agenda_paciente' })
+	patient?: Patient
 }
