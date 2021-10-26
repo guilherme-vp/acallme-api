b5ea291e4cf9b26c3b912f6f186e24f528155dc1
Guilherme Vieira <guivpw68@gmail.com>
2021-10-26 02:05:07 -0300

    refactor: change folder name from entities to models

--- src/common/domain/entities/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './user.entity'
-export * from './user-formatted.entity'
-export * from './notification.entity'
--- src/common/domain/models/notification.model.ts
+++ src/common/domain/models/notification.model.ts
@@ -1,4 +1,4 @@
-export interface Notification {
+export interface NotificationModel {
 	id: string
 	scheduleId: number
 	avatar?: string
