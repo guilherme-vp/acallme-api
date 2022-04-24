1610c4ca32df3d91f5150783963d2556c8ff305f
Guilherme Vieira <guivpw68@gmail.com>
2022-04-23 22:52:23 -0300

    refactor: update handshake logic

--- src/modules/specialists/decorators/specialist-ws.decorator.ts
+++ src/modules/specialists/decorators/specialist-ws.decorator.ts
@@ -1,30 +1,16 @@
-import { SECRET } from '@common/constants/configuration'
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
-import { WsException } from '@nestjs/websockets'
-import jwt from 'jsonwebtoken'
 import { Socket } from 'socket.io'
 
 import { Specialist } from '../entities'
 
 export const SpecialistWs = createParamDecorator(
 	(data: keyof Specialist, context: ExecutionContext) => {
-		try {
-			const request = context.switchToWs().getClient() as Socket
+		const request = context.switchToWs().getClient() as Socket
 
-			const { auth, headers } = request.handshake
+		const { auth } = request.handshake
 
-			if (auth) {
-				return data ? auth[data] : auth
-			}
-
-			const token = headers.authorization?.split(' ')
-
-			if (token && token[1]) {
-				const decoded: any = jwt.verify(token[1], SECRET)
-				return data ? decoded[data] : decoded.user
-			}
-		} catch (e) {
-			throw new WsException('Invalid or expired token')
+		if (auth) {
+			return data ? auth[data] : auth
 		}
 	}
 )
